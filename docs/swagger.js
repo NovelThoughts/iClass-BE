3e838b793978dc1cc512195c1f595bd176d65149
Apurva <apurvanagar@gmail.com>
2022-09-12 15:33:50 +0530

    fixes#2 : changed relative path to source file

--- docs/swagger.js
+++ docs/swagger.js
@@ -9,11 +9,11 @@ const doc = {
   schemes: ["http"],
 };
 
-const outputFile = "./swagger-output.json";
-const endpointsFiles = ["../index.js"];
+const outputFile = "./docs/swagger-output.json";
+const endpointsFiles = ["./index.js"];
 
 /* NOTE: if you use the express Router, you must pass in the
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen()(outputFile, endpointsFiles, doc);
